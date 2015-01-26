0e771d95b450cf8320da9e119a5ca82b3ace9927
Pedro Jordano <pedroj@me.com>
2015-01-26 22:03:56 +0100

    Added isolines labels with annotate.

--- sde_testing.R
+++ sde_testing.R
@@ -66,5 +66,63 @@ legend("topright", title="Functional group",
     c("Large birds","Thrushes","Warblers","Small muscicapids","Others"),
     pch=c(17,15,6,2,5), horiz=F, ncol=1)
 #---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+# Code with ggplot2
+# This plots the isolines (code prototype by Bernardo Santos.)
+#
+nlines <- 10 # number of isolines wanted
+# slope of a straight line linking (left,bottom) to (right,above) 
+# corners of the graphic
+alfa <- max(sde$eff_per_vis)/max(sde$visits)
+
+# sequence of (nlines) regular spaced x values for the isoclines
+xval <- seq(0, max(sde$visits), 
+    length.out=(nlines+1))[2:(nlines+1)] 
+isoc <- (xval*xval*alfa) # values of the isoclines
+
+vis1<-seq(0,max(sde$visits),length.out=1000)
+
+#---------------------------------------------------------------------------
+pp<- as.data.frame(vis1) # Build dataset for within loop plot.
+for(i in 1:nlines)
+{
+    pp<- cbind(pp, isoc[i]/vis1)
+}    
+
+# Main plot ----------------------------------------------------------------
+require(devtools)
+require(ggplot2)
+# mytheme_bw.R
+devtools::source_gist("https://gist.github.com/b843fbafa3af8f408972")
+#
+p1<- ggplot(sde, aes(x=visits, y=eff_per_vis)) + 
+    geom_point(shape=sde$group, size=5) +
+    geom_text(size=4, label=sde$animal,hjust=0.5, vjust=1.9) +
+    mytheme_bw()
+
+# Adding isolines
+labelx<- rep(0.8*max(sde$visits), nlines)
+labely<- as.vector(t(pp[800,2:11]))
+
+for(i in 1:nlines+1){ 
+    #labely<- isoc[i]/(0.8*max(sde$eff_per_vis)
+#    labely<- pp[,i][800]
+    p1= p1 + geom_line(aes(x, y), 
+        data= data.frame(x= pp$vis1, y= pp[,i]), 
+        col="blue", size = 0.25, alpha= 0.6) + 
+        ylim(0, max(sde$eff_per_vis)) +
+        xlab("Visit rate (/10h)") + 
+        ylab("Effectiveness/visit (No. fruits handled/vis)")  # +
+#        geom_text(aes(), data= NULL, x= labelx, y= labely, 
+#            label = paste("QC = ", round(isoc[i], digits=1)),
+#            size = 4, colour = "red")
+}
+p1 + annotate("text", x= labelx, y= labely,
+        label=paste("QC= ", round(isoc,1)), 
+        size=4, colour="red", hjust=0)
+
+print(p1) 
+#---------------------------------------------------------------------------
 
 
